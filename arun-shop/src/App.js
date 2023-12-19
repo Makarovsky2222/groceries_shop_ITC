239c210eff563dbe493eac2c066307889073790f
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2023-12-19 11:10:31 +0700

    Improved navbar

--- arun-shop/src/App.js
+++ arun-shop/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Order from './pages/Order';
 import Navigation from './components/Navigation';
@@ -13,16 +13,15 @@ const App = () => {
       <div>
         <Navigation />
 
-        <hr />
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/order" element={<Order />} />
-          <Route exact path="/product" element={<ProductList />} />
-
-        </Routes>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/order" element={<Order />} />
+            <Route exact path="/product" element={<ProductList />} />
+          </Routes>
+        </main>
       </div>
-      
     </Router>
   );
 };
