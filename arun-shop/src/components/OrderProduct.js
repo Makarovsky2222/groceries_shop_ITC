28fdab82732160643c53cf5bd1d80c6f141b5325
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2023-12-22 14:30:56 +0700

    Basic code of order functionnality

--- arun-shop/src/components/OrderProduct.js
+++ arun-shop/src/components/OrderProduct.js
@@ -1,43 +1,129 @@
-// Order.js
-import React, { useState } from 'react';
+// Import necessary dependencies
+import React, { useState, useEffect } from 'react';
+import ProductList from '../pages/ProductList';
 
 const OrderProduct = () => {
-  const [orderItems, setOrderItems] = useState([]);
+  // Define state for order details
+  const [orderDetails, setOrderDetails] = useState({
+    category: '', // selected category
+    product: '', // selected product
+    emissionDate: '',
+    estimatedArrivalDate: '',
+    author: '',
+  });
+
+  // Define state for categories and products
+  // const [categories, setCategories] = useState([]);
+  const categories = [
+    {
+      categoryName: "Fruits",
+      products: [
+        { name: "Apple", price: 1.5 },
+        { name: "Banana", price: 0.75 },
+        { name: "Orange", price: 1.2 },
+      ],
+    },
+    {
+      categoryName: "Vegetables",
+      products: [
+        { name: "Carrot", price: 0.9 },
+        { name: "Broccoli", price: 2.0 },
+        { name: "Tomato", price: 1.0 },
+      ],
+    },
+    // Add more categories as needed
+  ];
+
+  // Function to flatten the categories into an array of products
+  const flattenCategories = (categoriesArray) => {
+    return categoriesArray.reduce((products, category) => {
+      return [...products, ...category.products];
+    }, []);
+  };
+
+  // Function to fetch categories from ProductList
+  const fetchCategories = () => {
+    // Set categories state with the categories array from ProductList
+    //setCategories(ProductList.categories);
+  };
+
+  // Function to update order details when user selects a category or product
+  const handleSelectChange = (e) => {
+    const { name, value } = e.target;
+    setOrderDetails((prevDetails) => ({ ...prevDetails, [name]: value }));
+  };
+
+  // Function to handle form submission (creating a new order)
+  const handleOrderSubmit = async (e) => {
+    e.preventDefault();
+
+    // with functionnal back-end
+    // const response = await createOrderAPI(orderDetails);
+    // setOrderDetails(response.data);
+  };
+
+  // Use useEffect to fetch categories when the component mounts
+  useEffect(() => {
+    fetchCategories();
+  }, []); // The empty dependency array ensures that the effect runs only once when the component mounts
 
   return (
     <div>
-      <h2>Your Order</h2>
-      {orderItems.length === 0 ? (
-        <p>Your order is empty. Start adding items!</p>
-      ) : (
-        <ul>
-          {orderItems.map((item, index) => (
-            <li key={index}>
-              {item.productName} - ${item.price.toFixed(2)}
-            </li>
-          ))}
-        </ul>
-      )}
-      {/* Assume you have a list of products to choose from */}
-      <h3>Available Products</h3>
-      <ul>
-        <li>
-          <button onClick={() => setOrderItems([...orderItems, { productName: 'Apple', price: 1.5 }])}>
-            Add Apple
-          </button>
-        </li>
-        <li>
-          <button onClick={() => setOrderItems([...orderItems, { productName: 'Banana', price: 0.75 }])}>
-            Add Banana
-          </button>
-        </li>
-        <li>
-          <button onClick={() => setOrderItems([...orderItems, { productName: 'Orange', price: 1.2 }])}>
-            Add Orange
-          </button>
-        </li>
-        {/* Add more products as needed */}
-      </ul>
+      <h2>Create a New Order</h2>
+      <form onSubmit={handleOrderSubmit}>
+        {/* Dropdown for selecting category */}
+        <label>
+          Select Category:
+          <select
+            name="category"
+            value={orderDetails.category}
+            onChange={handleSelectChange}
+          >
+            <option value="">Select Category</option>
+            {categories.map((category) => (
+              <option key={category.categoryName} value={category.categoryName}>
+                {category.categoryName}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        {/* Dropdown for selecting product */}
+        <label>
+          Select Product:
+          <select
+            name="product"
+            value={orderDetails.product}
+            onChange={handleSelectChange}
+          >
+            <option value="">Select Product</option>
+            {orderDetails.category &&
+              flattenCategories(categories)
+                .filter((product) => product.categoryName === orderDetails.category)
+                .map((product) => (
+                  <option key={product.name} value={product.name}>
+                    {product.name}
+                  </option>
+                ))}
+          </select>
+        </label>
+
+        {/* Add input fields for emission date, estimated arrival date, and author */}
+        {/* Use appropriate input types and labels */}
+        {/* Example: */}
+        <label>
+          Emission Date:
+          <input
+            type="text"
+            name="emissionDate"
+            value={orderDetails.emissionDate}
+            onChange={handleSelectChange}
+          />
+        </label>
+
+        {/* Repeat for other input fields */}
+        <button type="submit">Submit Order</button>
+      </form>
     </div>
   );
 };
