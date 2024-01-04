ed10c3e8ab25440948f14847be6c6c68ee37bc5c
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2024-01-04 15:37:33 +0700

    Added sorting by category in Order Module

--- arun-shop/src/components/OrderModule/OrderProduct.js
+++ arun-shop/src/components/OrderModule/OrderProduct.js
@@ -1,18 +1,18 @@
-// OrderProduct.js
 import React, { useMemo, useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 import Category from "../Category";
 import Caddy from "./Caddy";
-import SearchBar from "./SearchBar";  // Import the new SearchBar component
+import SearchBar from "./SearchBar";
 import categoriesData from "./categories.json";
 import "./Styling/OrderProduct.css";
-import "./Styling/SearchBar.css"
+import "./Styling/OrderProductCategory.css"
 
 const OrderProduct = () => {
   const initialCategories = useMemo(() => categoriesData.categories, []);
   const [filteredCategories, setFilteredCategories] = useState(initialCategories);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   const handleSearch = (searchTerm) => {
     const filtered = initialCategories.filter((category) => {
@@ -22,14 +22,34 @@ const OrderProduct = () => {
       );
     });
     setFilteredCategories(filtered);
+    setSelectedCategory(null); // Reset selected category when searching
+  };
+
+  const handleCategoryClick = (categoryName) => {
+    const clickedCategory = initialCategories.find((category) => category.categoryName === categoryName);
+    if (clickedCategory) {
+      setFilteredCategories([clickedCategory]);
+      setSelectedCategory(clickedCategory.categoryName);
+    }
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="order-dashboard">
-        <div className="search-bar">
-         <SearchBar onSearch={handleSearch} />
-       </div>
+        <div className="search-bar-wrapper">
+          <SearchBar onSearch={handleSearch} />
+        </div>
+        <div className="category-wrapper">
+          {initialCategories.map((category) => (
+            <div
+              key={category.categoryName}
+              className={`category-card ${selectedCategory === category.categoryName ? "selected" : ""}`}
+              onClick={() => handleCategoryClick(category.categoryName)}
+            >
+              {category.categoryName}
+            </div>
+          ))}
+        </div>
         {filteredCategories.map((category) => (
           <Category key={category.categoryName} {...category} />
         ))}
