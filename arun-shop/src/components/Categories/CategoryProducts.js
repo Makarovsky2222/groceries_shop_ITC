87253ca6116f88467ef83b0b50aab7902d7e6ac9
SOPHART Chhordaphea <106377148+chhordaphea-s@users.noreply.github.com>
2024-01-10 15:55:06 +0700

    update resource + finish each info of product (without edit dlt icons)

--- arun-shop/src/components/Categories/CategoryProducts.js
+++ arun-shop/src/components/Categories/CategoryProducts.js
@@ -7,30 +7,40 @@ function AllProductList() {
 
 
     return (
-        <div className="product-container">
+        <div className="action-on-product">
+            <div className="product-container">
 
-            <div className="each-product">
-                <p id="product-name">Beef USA</p>
+                <div className="each-product">
 
-                <div id="product-category">
-                    Vegetables
-                </div>
+                    <div className="product-name-and-pf">
+                        <img id="product-img" src="https://i5.walmartimages.com/seo/Fresh-Slicing-Tomato-Each_a1e8e44a-2b82-48ab-9c09-b68420f6954c.04f6e0e87807fc5457f57e3ec0770061.jpeg" alt="My Image" />  
+                        <p id="product-name">Tomato Aussie</p>
+                    </div>
 
-                <p id="product-price">10.00%</p>
+                    <div id="product-category">
+                        Vegetables
+                    </div>
 
-                <div id="product-description"> 
-                    Cherry Australia has two types of fruit: one with a hard skin, the other with a soft skin. The soft-skinned fruits are a bit larger than the hard-skinned ones and have a higher sugar content. These fruits are widely used in fruit juices and canned fruits 
-                </div>
-                
-                <p id="product-tax">0%</p>
-                
+                    <p id="product-price">10.00$</p>
 
-                <div className="edit-delete">
+                    <div id="product-description"> 
+                        Cherry Australia has two types of fruit: one with a hard skin, the other with a soft skin. The soft-skinned fruits are a bit larger than the hard-skinned ones and have a higher sugar content. These fruits are widely used in fruit juices and canned fruits 
+                    </div>
+                    
+                    <p id="product-tax">0%</p>
                     
                 </div>
-                
-            </div>
 
+                <div className="edit-delete">
+                    <button id="edit-button" type="button" >
+                        <img id="edit-img" src="../../Resources/icons/edit.svg" />
+                    </button>
+                    <button id="dlt-button" type="button" >
+                        <img id="dlt-img" src="../../Resources/icons/delete.svg" />
+                    </button>
+                </div>
+
+            </div>
 
         </div>
 
