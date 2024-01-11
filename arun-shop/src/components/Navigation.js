66806f341684a08a6bf550394757b18d7a5169d1
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2024-01-11 11:01:17 +0700

    Reworking nav sidebar

--- arun-shop/src/components/Navigation.js
+++ arun-shop/src/components/Navigation.js
@@ -1,12 +1,23 @@
 // Navigation.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navigation.css'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+import toplogo from "../Resources/logo/logo.svg";
+import orderlogo from "../Resources/icons/order.svg";
+import stocklogo from "../Resources/icons/stock.svg";
+import historylogo from "../Resources/icons/history.svg";
 
 const Navigation = () => {
+  const [isReduced, setReduced] = useState(false);
+
+  const toggleNavbar = () => {
+    setReduced(!isReduced);
+  };
   return (
-    <nav>
+    <nav className={`navbar ${isReduced ? "reduced" : ""}`}>
+      <div className="top-logo">
+        <img src={toplogo} alt="Top Logo" />
+      </div>
       <ul>
         <li>
           <Link to="/login">Login</Link>
@@ -14,20 +25,53 @@ const Navigation = () => {
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/order">Order</Link>
-        </li>
-        <li>
-          <Link to="/history">History</Link>
-        </li>
-        <li>
-          <Link to="/product">Stock</Link>
-        </li>
+        {isReduced ? (
+          <li>
+            <Link to="/order">
+            <div className="logo">
+              <img src={orderlogo} alt="Order Logo" />
+            </div>
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <img src={orderlogo} alt="Order Logo" />
+            <Link to="/order">
+              <span>Order</span>
+              </Link>
+          </li>
+        )}
+        {isReduced ? (
+          <li>
+            <Link to="/history">
+              <img src={historylogo} alt="History Logo" />
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <img src={historylogo} alt="History Logo" />
+            <Link to="/history">History</Link>
+          </li>
+        )}
+        {isReduced ? (
+          <li>
+            <Link to="/product">
+              <img src={stocklogo} alt="Stock Logo" />
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <img src={stocklogo} alt="Stock Logo" />
+            <Link to="/product">Stock</Link>
+          </li>
+        )}
         <li>
           <Link to="/about">About</Link>
         </li>
-
       </ul>
+      <div className="toggle-btn" onClick={toggleNavbar}>
+        {isReduced ? '→' : '←'}
+      </div>
     </nav>
   );
 };
