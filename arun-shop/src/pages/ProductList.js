5d11e0fc194f2d60a5a6e6df753cf1c74a96590a
SOPHART Chhordaphea <106377148+chhordaphea-s@users.noreply.github.com>
2024-01-12 09:58:14 +0700

    added some changes

--- arun-shop/src/pages/ProductList.js
+++ arun-shop/src/pages/ProductList.js
@@ -2,39 +2,13 @@
 
   import React from "react";
   import AddProduct from "../components/AddProduct";
-  import Category from "../components/Category";
 
-  const ProductList = () => {
-      const categories = [
-          {
-            categoryName: "Fruits",
-            products: [
-              { name: "Apple", price: 1.5 },
-              { name: "Banana", price: 0.75 },
-              { name: "Orange", price: 1.2 },
-            ],
-          },
-          {
-            categoryName: "Vegetables",
-            products: [
-              { name: "Carrot", price: 0.9 },
-              { name: "Broccoli", price: 2.0 },
-              { name: "Tomato", price: 1.0 },
-            ],
-          },
-          // Add more categories as needed
-        ];
-      
+  function DisplayProduct() {
 
     return (
-      <div>
-        <h1>Groceries Store</h1>
-        {categories.map((category) => (
-          <Category key={category.categoryName} {...category} />
-        ))}
-        <AddProduct />
-      </div>
-    );
-  };
+      <div></div>
+    )
 
- export default ProductList;
+  }
+
+  export default DisplayProduct;
