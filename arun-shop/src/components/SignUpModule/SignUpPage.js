cd9b7bae0f422b34c05f53a9f769bc99796086f1
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2024-01-15 16:54:02 +0700

    Merge current Order and login features

---
+++ arun-shop/src/components/SignUpModule/SignUpPage.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import SignUpCSS from '../SignUpModule/SignUpPage.module.css'
+import { signup } from '../../services/Authentication.js'
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    uid: '',
+    username: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone_number: '',
+    register_date: '',
+    image_url: '',
+    password: '',
+    confirm_password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }; 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (formData.password != formData.confirm_password) {
+      alert("Password is not match!")
+      return
+    }
+
+    formData.register_date = new Date(0).toString();
+
+    const status = await signup(formData)
+    if (status) {
+      console.log('Form submitted:', formData);
+    }
+  };
+  return (
+    <form action='login'>
+         <div className={SignUpCSS.wrapper}>
+            <div className={SignUpCSS.signup}>
+                    <h1>Sign Up</h1>
+            </div>
+            <div className={SignUpCSS.all}>
+                <div className={SignUpCSS.left}>
+                    <img src={require('../../img/image 1.png')}></img>
+                </div>
+                <div className={SignUpCSS.right}>
+                    <div className={SignUpCSS.inputbox}>
+                        <input type='uid' name='uid' placeholder='UID'/>
+                    </div>
+                    <div className={SignUpCSS.name}>
+                        <div className={SignUpCSS.inputbox1}>
+                            <input type='F-name' name='F-name' placeholder='First Name'/>
+                            <input type='L-name' name='L-name' placeholder='Last Name'/>
+                        </div>
+                    </div>
+                    <div className={SignUpCSS.inputbox}>
+                        <input type='email' name='email' placeholder='Email'/>
+                    </div>
+                    <div className={SignUpCSS.inputbox}>
+                        <input type='ph-num' name='ph num' placeholder='Phone Number'/>
+                    </div>
+                    <div className={SignUpCSS.inputbox}>
+                        <input type='password' name='password' placeholder='Password'/>
+                    </div>
+                    <div className={SignUpCSS.inputbox}>
+                        <input type='con-pass' name='con-pass' placeholder='Confirm Password'/>
+                    </div>
+                    <div className={SignUpCSS.action}>
+                        <p>Go back to <a href='/login'>Login</a></p>
+                        <button type='submit'>Create Account</button>
+                    </div>
+            </div>
+            </div>
+            
+        </div>
+    </form>
+  );
+}
+
+export default SignUp;
