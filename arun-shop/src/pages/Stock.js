f17aa5b6efc54ff9170d039b6065ecf3d840647c
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2024-01-18 08:57:48 +0700

    Corrected imports

--- arun-shop/src/pages/Stock.js
+++ arun-shop/src/pages/Stock.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AllStockIn from '../components/StockModule/AllStockIn';
-import ListStock from '../components/StockModule/ListStock';
 import StockProduct from '../components/StockModule/StockProduct';
 import NewStockProduct from '../components/StockModule/NewStockProduct';
 import SelectProduct from '../components/StockModule/SelectProduct';
 import Searchbar from'../components/General/Searchbar';
 import EditStock from '../components/StockModule/EditStock';
+import ListStock from '../components/StockModule/ListStock';
 const   Stock =() => {
   return (
     <div>
