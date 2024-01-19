dcf8ef68aca5cd96d8faab71ff8bb89ed747843a
Makarovsky2222 <arthur.furet@insa-rennes.fr>
2024-01-19 08:15:12 +0700

    manage categories button

--- arun-shop/src/components/Categories/BlockCategory.js
+++ arun-shop/src/components/Categories/BlockCategory.js
@@ -1,60 +1,58 @@
-import React from "react";
-import './Styling/BlockCategory.css';
-import allproductimage from '../../Resources/products/categories/allProducts.png'
-import vegimage from '../../Resources/products/categories/vegetables.png'
-import fruitimage from '../../Resources/products/categories/fruits.png'
-import ingreimage from '../../Resources/products/categories/ingredients.png'
-import meatimage from '../../Resources/products/categories/meats.png'
-import createimg from '../../Resources/products/categories/create.svg'
+import React, { useState } from "react";
+import "./Styling/BlockCategory.css";
+import allproductimage from "../../Resources/products/categories/allProducts.png";
+import vegimage from "../../Resources/products/categories/vegetables.png";
+import fruitimage from "../../Resources/products/categories/fruits.png";
+import ingreimage from "../../Resources/products/categories/ingredients.png";
+import meatimage from "../../Resources/products/categories/meats.png";
+import createimg from "../../Resources/products/categories/create.svg";
 
-
-
-
-
+import RegisterCategory from "./RegisterCategory"; // Import the RegisterCategory component
 
 function BlockCategory() {
-    return(
+  const [showRegisterCategory, setShowRegisterCategory] = useState(false);
 
-        <div className="categories-section">
-            <div className="each-block">
-                <img id="img" src={allproductimage} />
-                <p id="text" >All Products</p>
-            </div>
+  const handleNewCategoryClick = () => {
+    setShowRegisterCategory(true);
+  };
 
-            <div className="veg-block">
-                <img id="img" src={vegimage} />
-                <p id="veg-text" >Vegetables</p>
-            </div>
+  return (
+    <div className="categories-section">
+      {/* Existing categories */}
+      <div className="each-block">
+        <img id="img" src={allproductimage} alt="All Products" />
+        <p id="text">All Products</p>
+      </div>
 
-            <div className="fruit-block">
-                <img id="img" src={fruitimage} />
-                <p id="veg-text" >Fruits</p>
-            </div>
+      <div className="veg-block">
+        <img id="img" src={vegimage} />
+        <p id="veg-text">Vegetables</p>
+      </div>
 
-            <div className="ingre-block">
-                <img id="img" src={ingreimage} />
-                <p id="veg-text" >Ingredients</p>
-            </div>
-            
-            <div className="meat-block">
-                <img id="img" src={meatimage} />
-                <p id="veg-text" >Meats</p>
-            </div>
+      <div className="fruit-block">
+        <img id="img" src={fruitimage} />
+        <p id="veg-text">Fruits</p>
+      </div>
 
+      <div className="ingre-block">
+        <img id="img" src={ingreimage} />
+        <p id="veg-text">Ingredients</p>
+      </div>
 
+      <div className="meat-block">
+        <img id="img" src={meatimage} />
+        <p id="veg-text">Meats</p>
+      </div>
+      {/* New Category block */}
+      <div className="new-block" onClick={handleNewCategoryClick}>
+        <img id="img" src={createimg} alt="New Category" />
+        <p id="veg-text">New Category</p>
+      </div>
 
-
-            <div className="new-block">
-                <img id="img" src={createimg} />
-                <p id="veg-text" >New Category</p>
-            </div>
-
-
-        </div>
-
-    )
-
-
+      {/* Conditionally render RegisterCategory component */}
+      {showRegisterCategory && <RegisterCategory />}
+    </div>
+  );
 }
 
 export default BlockCategory;
--- arun-shop/src/components/Categories/RegisterCategory.js
+++ arun-shop/src/components/Categories/RegisterCategory.js
@@ -1,38 +1,50 @@
-import React from 'react';
-import './Styling/RegisterCategory.css';
-import imageCancel from '../../Resources/icons/cancel.svg';
-import { UploadImage } from './UploadImage';
-
+import React, { useState } from "react";
+import "./Styling/RegisterCategory.css";
+import imageCancel from "../../Resources/icons/cancel.svg";
+import { UploadImage } from "./UploadImage";
 
 function RegisterCategory() {
-    return (
+  const [newCategory, setNewCategory] = useState("");
 
-        <div className="control-section">
+  const handleCategoryNameChange = (event) => {
+    setNewCategory(event.target.value);
+  };
 
-            <div className='header'>
-                <button id='cancel-button'>
-                    <img id='imgCancel' src={imageCancel} />
-                </button>
-                <p id='registerText'>Category Registration</p>
-            </div>
+  const handleSaveCategory = () => {
+    // TODO: Add logic to save the new category (e.g., make an API call or update state)
+    console.log("Saving category:", newCategory);
+    // Clear the input after saving
+    setNewCategory("");
+  };
 
-            <div className='image-container' >
-                <UploadImage  />
-            </div>
+  return (
+    <div className="control-section">
+      <div className="header">
+        <button id="cancel-button">
+          <img id="imgCancel" src={imageCancel} alt="Cancel" />
+        </button>
+        <p id="registerText">Category Registration</p>
+      </div>
 
-            <div className='input-info'>
-                <input id='product-name' type="text" placeholder="Category Name"></input>
-            </div>
+      <div className="image-container">
+        <UploadImage />
+      </div>
 
+      <div className="input-info">
+        <input
+          id="product-name"
+          type="text"
+          placeholder="Category Name"
+          value={newCategory}
+          onChange={handleCategoryNameChange}
+        />
+      </div>
 
-            <button id='saveinfo-button'>
-                SAVE
-            </button>
-
-        </div>
-
-    )
-
-
+      <button id="saveinfo-button" onClick={handleSaveCategory}>
+        SAVE
+      </button>
+    </div>
+  );
 }
-export default RegisterCategory
+
+export default RegisterCategory;
